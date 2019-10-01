b9d13b7f1883701fc93dcfa06ce769310b9a1039
Saket <21242528+khajuriasaket486@users.noreply.github.com>
2019-10-02 01:08:11 +0530

    Arping but ARP() has to be fixed for pdst=ip.

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import scapy.all as scapy
-import sys
-print('Python %s on %s' % (sys.version, sys.platform))
 def scan(ip):
     arp_request = scapy.ARP()
-    scapy.ls(arp_request)
-    arp_request.pdst=ip
-    scapy.ls(arp_request)
-    print(arp_request.summary())
+    scapy.ls(arp_request)   # or arp_request.show()
+    arp_request.pdst = ip
+#   scapy.ls(arp_request)
+#   print(arp_request.summary())   # prints ARP who is pdst says psrc
 
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+#   scapy.ls(broadcast)
+#   print(broadcast.summary())
 
-scan('192.168.1.0/24')
+    arp_request_broadcast = broadcast/arp_request
+#   print(arp_request_broadcast.summary())
+#   arp_request_broadcast.show()
+
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
+    print(unanswered.summary())
+scan('192.168.1.1/24')
